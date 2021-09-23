a9f9d057cf2b6ad6ce5310f3bf65af3c538ffde4
Omega-lv <pablolvpablolv@gmail.com>
2021-09-24 00:42:50 +0200

    trampeado mapa para poder trabajar mientras

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from random import *
 
+"""
+import numpy as np
+"""
 
 #FUNCION ENCARGADA DE IMPRIMRIR POR PANTALLA EL MAPA CON CELULAS VIVAS Y MUERTAS
 #SE LE PASA COMO PARAMETRO UNA MATRIZ
@@ -21,15 +24,25 @@ def plotea_mapas(mapa):
 seed(1)
 print("Juego de la vida")
 print("\n")
+"""
+#DE MOMENTO UTILIZAREMOS UNO GENERADO A PELO
+#MÁS ADELANTE BUSCAREMOS LA FORMA DE HACERLO GENERICO
 columnas= int(input("Introduce numero de columnas: "))
 print("\n")
 filas= int(input("Introduce numero de filas: "))
+"""
 contadorFilas=0
 contadorColumnas=0
 
 
 #CREACION DEL MAPA INICIAL
-mapa = [[0]*columnas]*filas
+#DE MOMENTO UTILIZAREMOS UNO GENERADO A PELO
+#MÁS ADELANTE BUSCAREMOS LA FORMA DE HACERLO GENERICO
+mapa = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
+#lineas de chapuceo aqui mientras trabajamos
+#QUITAR CUANDO QUERAMOS QUE EL INPUT DEL USUARIO VALGA DE ALGO
+columnas=8
+filas=6
 plotea_mapas(mapa)
 while contadorFilas<filas:
     contadorColumnas=0
@@ -43,13 +56,14 @@ while contadorFilas<filas:
     contadorFilas=contadorFilas+1
 
 #PINTAMOS EL MAPA INICIAL
+print("El mapa inicial")
 plotea_mapas(mapa)
 
 """
 #PASO A LA SIGUIENTE RONDA
 usuario=input("Introduzca 'n' para ver la siguiente generación o 'q' para finalizar el programa")
 if usuario=="q":
-    exit()
+exit()
 
 #toca ahora ver como calculamos la siguiente generacion
 #quiza definir un metodo que lo calcule sea lo más cómodo
