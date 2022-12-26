4fd603dee44db4a4aedd60d7b2a8d8d23cbcef1c
Omega-lv <pablolvpablolv@gmail.com>
2022-12-26 14:53:10 +0100

    motor funcional

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 void printea_matriz(int** matriz, int size){
 
     int i;
     int j;
+    printf("\n");
     for (i=0; i<size; i++){
         printf("|");
         for (j=0; j<size; j++){
@@ -13,7 +15,42 @@ void printea_matriz(int** matriz, int size){
         }
         printf("\n");
     }
+    printf("\n");
 
+
+}
+
+void calculo_paso(int** matriz_visible, int** matriz_support, int size){
+
+    int i;
+    int j;
+    int i_dos;
+    int j_dos;
+    int contador_vivos;
+
+    for (i=0; i<size; i++){
+        for (j=0; j<size; j++){
+            contador_vivos=0;
+            //resolvemos caso general
+            for (i_dos=i-1;i_dos<=i+1;i_dos++){
+                for (j_dos=j-1;j_dos<=j+1;j_dos++){
+                    if (i_dos>=size || j_dos>=size || i_dos<0 || j_dos<0 || (i_dos==i && j_dos==j)){
+                        continue;
+                    }
+                    if (matriz_visible[i_dos][j_dos]==1){
+                        contador_vivos++;
+                    }
+                }
+            }
+            printf("para la posicion [%d][%d] el valor de vivos es= %d \n",i, j, contador_vivos);
+            if ((matriz_visible[i][j]==1 && (contador_vivos==2 || contador_vivos==3)) || (matriz_visible[i][j]==0 && contador_vivos==3)){
+                matriz_support[i][j]=1;
+            }
+            else {
+                matriz_support[i][j]=0;
+            }
+        }
+    }
 
 }
 
@@ -25,7 +62,7 @@ int main() {
     Reglas del juego de la vida:
     
     Un 1 se convierte en 0 si:
-        -A su alrededor hay >2 o >3 1s a su alrededor
+        -A su alrededor hay <2 o >3 1s a su alrededor
 
     Un 0 se convierte en 1 si:
         -A su alrededor hay 3 1s
@@ -53,8 +90,9 @@ int main() {
    //comienza ejecucion del programa
    //preguntamos al man que tamaño de matriz quiere
 
-   int size_matriz=0;
-   int i=0;
+   int size_matriz;
+   int i;
+   int j;
    
 
    /* descomentar cuando hayamos perfeccionado el método de entrada
@@ -78,7 +116,8 @@ int main() {
 
     for (i=0; i<size_matriz; i++){
         Matriz_visible[i]=(int *)calloc(size_matriz,sizeof(int));
-        if (Matriz_visible[i]==NULL){
+        Matriz_apoyo[i]=(int *)calloc(size_matriz,sizeof(int));
+        if (Matriz_visible[i]==NULL || Matriz_apoyo[i]==NULL){
             printf("Peto reservando las columnas");
             return 1;
         }
@@ -92,6 +131,7 @@ int main() {
     //ponemos 1s de forma manual en la matriz
     //vamos a copiar el diseño original del juego para poder comprobar
     Matriz_visible[3][4]=1;
+    Matriz_visible[3][5]=1;
     Matriz_visible[4][5]=1;
     Matriz_visible[5][5]=1;
     Matriz_visible[5][4]=1;
@@ -100,6 +140,18 @@ int main() {
     //printeamos por consola de momento para ver que todo funciona correctamente
     printea_matriz(Matriz_visible, size_matriz);
 
+    //calculo del siguiente paso
+    calculo_paso(Matriz_visible, Matriz_apoyo, size_matriz);
+    //copiamos la matriz a mano a ver si es eso
+    for (i=0; i<size_matriz; i++){
+        for (j=0; j<size_matriz;j++){
+            Matriz_visible[i][j]=Matriz_apoyo[i][j];
+        }
+    }
+    printf("\n Se ha realizado el calculo del paso y la copia de la matriz \n");
+
+    //volvemos a printear para que se vea el siguiente paso
+    printea_matriz(Matriz_visible, size_matriz);
 
     //liberamos la memoria de las matrices
     //de nuevo, cuando el futuro cambiemos a row-major o lo que sea habrá que alterar esto
